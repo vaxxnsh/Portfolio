7dd33dce4e13e9c65a51888032a231c91f18f323
vaxxnsh <139635892+vaxxnsh@users.noreply.github.com>
2025-07-28 04:14:25 +0530

    Update SpotifyPlayer.tsx

--- components/SpotifyPlayer.tsx
+++ components/SpotifyPlayer.tsx
@@ -5,7 +5,7 @@ export default function SpotifyPlayer() {
     <div className="relative z-50 w-full mt-5 overflow-hidden rounded-xl">
       <IconWrapper className="w-full block p-2 bg-transparent">
         <iframe
-          src="https://open.spotify.com/embed/track/3HlnDsfP4g2pRpzZrueI4F?utm_source=generator&theme=0"
+          src="https://open.spotify.com/embed/track/4JlB9CqV7CSbweHTSOZ93w?utm_source=generator&theme=0"
           width="100%"
           height="80"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
