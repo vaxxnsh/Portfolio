17acbf71a93af22119b6f1f5e87fa19c8958eee8
vaxxnsh <139635892+vaxxnsh@users.noreply.github.com>
2025-07-31 04:20:37 +0530

    Update SpotifyPlayer.tsx
    it's hard to find recruiter freindly rap songs. guess have to stick with this one.

--- components/SpotifyPlayer.tsx
+++ components/SpotifyPlayer.tsx
@@ -5,7 +5,7 @@ export default function SpotifyPlayer() {
     <div className="relative z-50 w-full mt-5 overflow-hidden rounded-xl">
       <IconWrapper className="w-full block p-2 bg-transparent">
         <iframe
-          src="https://open.spotify.com/embed/track/3pjUyVbFmM96tYhSaKJwTt?utm_source=generator&theme=0"
+          src="https://open.spotify.com/embed/track/16umSNZfofRpDqTvf8DIAo?utm_source=generator&theme=0"
           width="100%"
           height="80"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
